23d1a8a97aab78eb97cf0a1db474e02d5feff3fe
Maku Uwasan <muwasan@gmail.com>
2021-12-05 12:15:25 +0000

    ready to containerize

--- blog/config.py
+++ blog/config.py
@@ -1,16 +1,11 @@
 import os
-import json
-
-with open('/etc/flaskblogconfig.json') as config_file:
-    config = json.load(config_file)
-
 
 class Config:
-    SECRET_KEY = config.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config.get('USER_EMAIL')
-    MAIL_PASSWORD =config.get('USER_PASSWORD')
+    MAIL_USERNAME = os.environ.get('USER_EMAIL')
+    MAIL_PASSWORD = os.environ.get('USER_PASSWORD')
 
